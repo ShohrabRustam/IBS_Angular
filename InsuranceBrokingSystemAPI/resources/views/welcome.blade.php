d4b4de7b4d197a05170b9da7999c416b9348868e
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-24 15:25:34 +0530

    add the laravel project

--- InsuranceBrokingSystemAPI/resources/views/welcome.blade.php
+++ InsuranceBrokingSystemAPI/resources/views/welcome.blade.php
@@ -8,7 +8,6 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="/css/main.css">
 
         <!-- Styles -->
         <style>
